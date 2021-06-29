252c47df0814a0297cfc25a9c7ab12ff40aa7f06
Pramesh <prameshshrestha455@gmail.com>
2021-06-28 20:50:52 -0700

    adding

---
+++ includes/banner.php
@@ -0,0 +1,45 @@
+<div class="slider-area">
+
+<!-- Main Slider -->
+<div class="swiper-container main-slider swiper-arrow with-bg_white">
+    <div class="swiper-wrapper">
+        <div class="swiper-slide animation-style-01">
+            <div class="slide-inner bg-height" data-bg-image="assets/images/slider/bg/slider1.jpg" >
+                <div class="container">
+                    <div class="slide-content text-white">
+                        
+                        <h2 class="title mb-3">Glasses For Men And Women</h2>
+                        <p class="short-desc different-width mb-10">Exclusive Exchange Offer & 30% Off In This Week</p>
+                        <div class="button-wrap">
+                            <a class="btn btn-custom-size lg-size btn-primary" href="shop-female.php">Shop Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="swiper-slide animation-style-01">
+            <div class="slide-inner bg-height" data-bg-image="assets/images/slider/bg/slider4.jpeg" >
+                <div class="container">
+                    <div class="slide-content text-white">
+                      
+                        <h2 class="title mb-3">Glasses For Men And Women</h2>
+                        <p class="short-desc different-width mb-10">Exclusive Exchange Offer & 30% Off In This Week</p>
+                        <div class="button-wrap">
+                            <a class="btn btn-custom-size lg-size btn-primary" href="shop-male.php">Shop Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Add Pagination -->
+    <div class="swiper-pagination with-bg d-md-none"></div>
+
+    <!-- Custom Arrows -->
+    <div class="custom-arrow-wrap d-none d-md-block">
+        <div class="custom-button-prev"></div>
+        <div class="custom-button-next"></div>
+    </div>
+</div>
+
+</div>
--- index.php
+++ index.php
@@ -11,60 +11,19 @@ include('includes/header.php');?>
     padding-top: 50px;
     padding-bottom: 50px;
 }
-
+.banner_area{
+    width:90%;
+    margin:30px auto;
+}
 </style>
 
         <!-- Begin Slider Area -->
-        <div class="slider-area">
-
-            <!-- Main Slider -->
-            <div class="swiper-container main-slider swiper-arrow with-bg_white">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide animation-style-01">
-                        <div class="slide-inner bg-height" data-bg-image="assets/images/slider/bg/slider1.jpg" >
-                            <div class="container">
-                                <div class="slide-content text-white">
-                                    
-                                    <h2 class="title mb-3">Glasses For Men And Women</h2>
-                                    <p class="short-desc different-width mb-10">Exclusive Exchange Offer & 30% Off In This Week</p>
-                                    <div class="button-wrap">
-                                        <a class="btn btn-custom-size lg-size btn-primary" href="shop-female.php">Shop Now</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide animation-style-01">
-                        <div class="slide-inner bg-height" data-bg-image="assets/images/slider/bg/slider4.jpeg" >
-                            <div class="container">
-                                <div class="slide-content text-white">
-                                  
-                                    <h2 class="title mb-3">Glasses For Men And Women</h2>
-                                    <p class="short-desc different-width mb-10">Exclusive Exchange Offer & 30% Off In This Week</p>
-                                    <div class="button-wrap">
-                                        <a class="btn btn-custom-size lg-size btn-primary" href="shop-male.php">Shop Now</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Add Pagination -->
-                <div class="swiper-pagination with-bg d-md-none"></div>
-
-                <!-- Custom Arrows -->
-                <div class="custom-arrow-wrap d-none d-md-block">
-                    <div class="custom-button-prev"></div>
-                    <div class="custom-button-next"></div>
-                </div>
-            </div>
-
-        </div>
+       <?php include('includes/banner.php');?>
         <!-- Slider Area End Here -->
 
        <!-- Start Banner Area -->
 		<div class="banner_area">
-			<div class="row row--2">
+			<div class="row">
 
 				<div class="col-lg-4 col-sm-6 col-12">
 					<div class="banner">
@@ -90,7 +49,7 @@ include('includes/header.php');?>
 					<div class="banner">
 						<div class="thumb">
 							<a href="#">
-								<img src="assets\images\banner\banner-1.jpg" height="260px" alt="banner img">
+								<img src="assets\images\banner\banner-1.jpg" height="240px" alt="banner img">
 							</a>
 						</div>
 					</div>
